bae434614be95bd1a4f4deb5c06b2866c4e7da3a
Paul Martin <paul.y.martin@johnlewis.co.uk>
2022-01-01 13:07:07 +0000

    .toDomain() reads key into property

--- src/main/kotlin/datastore_mapper/to_domain/ToDomain.kt
+++ src/main/kotlin/datastore_mapper/to_domain/ToDomain.kt
@@ -3,24 +3,37 @@ package datastore_mapper.to_domain
 import com.google.cloud.datastore.Entity
 import kotlin.reflect.KClass
 import kotlin.reflect.KParameter
+import kotlin.reflect.KProperty1
 import kotlin.reflect.full.createType
 import kotlin.reflect.full.primaryConstructor
 import kotlin.reflect.full.withNullability
 
-fun <T : Any> Entity.toDomain(targetClass: KClass<T>): T {
+fun <T : Any> Entity.toDomain(targetClass: KClass<T>, keyProperty: KProperty1<T, Any>): T {
     val primaryConstructor = targetClass.primaryConstructor!!
 
     val constructorArguments = primaryConstructor
         .parameters
-        .associateWith(this::getValue)
+        .associateWith {
+            if (it.name == keyProperty.name) getKeyValue(it)
+            else getValue(it)
+        }
 
     return primaryConstructor.callBy(constructorArguments)
 }
+
+private fun Entity.getKeyValue(parameter: KParameter) =
+    // TODO this will return null if key identifier is of the wrong type, and then calling the constructor fails. make a nice error message?
+    when (parameter.type.withNullability(false)) {
+        String::class.createType() -> key.name
+        Int::class.createType() -> key.id.toInt()
+        Long::class.createType() -> key.id
+        else -> TODO("Support for key of type ${parameter.type} not implemented")
+    }
 
 private fun Entity.getValue(parameter: KParameter) =
     when (parameter.type.withNullability(false)) {
         String::class.createType() -> this.getOptionalString(parameter.name)
         Int::class.createType() -> this.getOptionalInt(parameter.name)
         Boolean::class.createType() -> this.getOptionalBoolean(parameter.name)
-        else -> TODO("Support for ${parameter.type} not implemented")
+        else -> TODO("Support for property of type ${parameter.type} not implemented")
     }
--- src/test/kotlin/datastore_mapper/to_domain/ToDomainKtTest.kt
+++ src/test/kotlin/datastore_mapper/to_domain/ToDomainKtTest.kt
@@ -4,80 +4,129 @@ import com.google.cloud.datastore.Entity
 import com.google.cloud.datastore.Key
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
-import io.mockk.mockk
 
 class ToDomainKtTest : StringSpec({
-    val mockKey = mockk<Key>()
+    "can read a string id into a given property" {
+        data class TestClass(val id: String)
+
+        val entity = Entity.newBuilder(createKey("my-id")).build()
+
+        val result = entity.toDomain(TestClass::class, TestClass::id)
+
+        result shouldBe TestClass(id = "my-id")
+    }
+
+    "can read a numerical id into a property of type Int" {
+        data class TestClass(val id: Int)
+
+        val entity = Entity.newBuilder(createKey(123456)).build()
+
+        val result = entity.toDomain(TestClass::class, TestClass::id)
+
+        result shouldBe TestClass(id = 123456)
+    }
+
+    "can read a numerical id into a property of type Long" {
+        data class TestClass(val id: Long)
+
+        val entity = Entity.newBuilder(createKey(123456)).build()
+
+        val result = entity.toDomain(TestClass::class, TestClass::id)
+
+        result shouldBe TestClass(id = 123456)
+    }
 
     "can convert a String parameter" {
-        data class TestClass(val stringProperty: String)
+        data class TestClass(val id: String, val stringProperty: String)
 
-        val entity = Entity.newBuilder(mockKey).set("stringProperty", "Foo").build()
+        val entity = Entity.newBuilder(createKey("id")).set("stringProperty", "Foo").build()
 
-        entity.toDomain(TestClass::class) shouldBe TestClass(stringProperty = "Foo")
+        val result = entity.toDomain(TestClass::class, TestClass::id)
+
+        result shouldBe TestClass(id = "id", stringProperty = "Foo")
     }
 
     "can convert an optional String parameter" {
-        data class TestClass(val stringProperty: String?)
+        data class TestClass(val id: String, val stringProperty: String?)
 
-        val entity = Entity.newBuilder(mockKey).set("stringProperty", "Foo").build()
+        val entity = Entity.newBuilder(createKey("id")).set("stringProperty", "Foo").build()
 
-        entity.toDomain(TestClass::class) shouldBe TestClass(stringProperty = "Foo")
+        val result = entity.toDomain(TestClass::class, TestClass::id)
+
+        result shouldBe TestClass(id = "id", stringProperty = "Foo")
     }
 
     "sets an optional String parameter to null if not present on the entity" {
-        data class TestClass(val stringProperty: String?)
+        data class TestClass(val id: String, val stringProperty: String?)
 
-        val entity = Entity.newBuilder(mockKey).build()
+        val entity = Entity.newBuilder(createKey("id")).build()
 
-        entity.toDomain(TestClass::class) shouldBe TestClass(stringProperty = null)
+        val result = entity.toDomain(TestClass::class, TestClass::id)
+
+        result shouldBe TestClass(id = "id", stringProperty = null)
     }
 
     "can convert an Int parameter" {
-        data class TestClass(val intParameter: Int)
+        data class TestClass(val id: String, val intParameter: Int)
 
-        val entity = Entity.newBuilder(mockKey).set("intParameter", 42L).build()
+        val entity = Entity.newBuilder(createKey("id")).set("intParameter", 42L).build()
 
-        entity.toDomain(TestClass::class) shouldBe TestClass(intParameter = 42)
+        val result = entity.toDomain(TestClass::class, TestClass::id)
+
+        result shouldBe TestClass(id = "id", intParameter = 42)
     }
 
     "can convert an optional Int parameter" {
-        data class TestClass(val intParameter: Int?)
+        data class TestClass(val id: String, val intParameter: Int?)
 
-        val entity = Entity.newBuilder(mockKey).set("intParameter", 42L).build()
+        val entity = Entity.newBuilder(createKey("id")).set("intParameter", 42L).build()
 
-        entity.toDomain(TestClass::class) shouldBe TestClass(intParameter = 42)
+        val result = entity.toDomain(TestClass::class, TestClass::id)
+
+        result shouldBe TestClass(id = "id", intParameter = 42)
     }
 
     "sets an optional Int parameter to null if not present on the entity" {
-        data class TestClass(val intParameter: Int?)
+        data class TestClass(val id: String, val intParameter: Int?)
 
-        val entity = Entity.newBuilder(mockKey).build()
+        val entity = Entity.newBuilder(createKey("id")).build()
 
-        entity.toDomain(TestClass::class) shouldBe TestClass(intParameter = null)
+        val result = entity.toDomain(TestClass::class, TestClass::id)
+
+        result shouldBe TestClass(id = "id", intParameter = null)
     }
 
     "can convert a Boolean parameter" {
-        data class TestClass(val booleanParameter: Boolean)
+        data class TestClass(val id: String, val booleanParameter: Boolean)
 
-        val entity = Entity.newBuilder(mockKey).set("booleanParameter", false).build()
+        val entity = Entity.newBuilder(createKey("id")).set("booleanParameter", false).build()
 
-        entity.toDomain(TestClass::class) shouldBe TestClass(booleanParameter = false)
+        val result = entity.toDomain(TestClass::class, TestClass::id)
+
+        result shouldBe TestClass(id = "id", booleanParameter = false)
     }
 
     "can convert an optional Boolean parameter" {
-        data class TestClass(val booleanParameter: Boolean?)
+        data class TestClass(val id: String, val booleanParameter: Boolean?)
 
-        val entity = Entity.newBuilder(mockKey).set("booleanParameter", false).build()
+        val entity = Entity.newBuilder(createKey("id")).set("booleanParameter", false).build()
 
-        entity.toDomain(TestClass::class) shouldBe TestClass(booleanParameter = false)
+        val result = entity.toDomain(TestClass::class, TestClass::id)
+
+        result shouldBe TestClass(id = "id", booleanParameter = false)
     }
 
     "sets an optional Boolean parameter to null if not present on the entity" {
-        data class TestClass(val booleanParameter: Boolean?)
+        data class TestClass(val id: String, val booleanParameter: Boolean?)
 
-        val entity = Entity.newBuilder(mockKey).build()
+        val entity = Entity.newBuilder(createKey("id")).build()
 
-        entity.toDomain(TestClass::class) shouldBe TestClass(booleanParameter = null)
+        val result = entity.toDomain(TestClass::class, TestClass::id)
+
+        result shouldBe TestClass(id = "id", booleanParameter = null)
     }
 })
+
+private fun createKey(name: String) = Key.newBuilder("project-id", "KindName", name).build()
+private fun createKey(id: Long) = Key.newBuilder("project-id", "KindName", id).build()
+
